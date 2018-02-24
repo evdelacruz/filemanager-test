4e874215e5e7274535fba29da25ad4ad2e34ff41
evdelacruz <evdelacruz@nauta.cu>
2018-02-24 17:49:51 -0500

    Added file system storage implementation.

--- src/main/java/org/test/filemanager/controller/GlobalAdvisor.java
+++ src/main/java/org/test/filemanager/controller/GlobalAdvisor.java
@@ -1,5 +1,6 @@
 package org.test.filemanager.controller;
 
+import org.apache.tomcat.util.http.fileupload.FileUploadBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -7,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartException;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -16,20 +16,31 @@ import static org.springframework.http.ResponseEntity.*;
 
 @ControllerAdvice
 public class GlobalAdvisor {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalAdvisor.class);
-    private static final Map<Class<? extends Exception>, Function<Exception, ResponseEntity>> EXCEPTIONS = new HashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(GlobalAdvisor.class);
+    private final Map<Class<? extends Exception>, Function<Exception, ResponseEntity>> exceptions = new HashMap<>();
 
-    static {
-        EXCEPTIONS.put(MultipartException.class, ex -> badRequest().body(ex.getMessage()));
+    public GlobalAdvisor() {
+        exceptions.put(MultipartException.class, ex -> this.getResponse((MultipartException) ex));
+        exceptions.put(IllegalArgumentException.class, ex -> badRequest().body(ex.getMessage()));
     }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity handleException(Exception ex) {
-        if (EXCEPTIONS.containsKey(ex.getClass())) {
-            LOGGER.debug("Expected exception handled !!!", ex);
-            return EXCEPTIONS.get(ex.getClass()).apply(ex);
+        if (exceptions.containsKey(ex.getClass())) {
+            logger.debug("Expected exception handled !!!", ex);
+            return exceptions.get(ex.getClass()).apply(ex);
         }
-        LOGGER.error("Unexpected exception handled !!!", ex);
+        logger.error("Unexpected exception handled !!!", ex);
         return status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Error");
     }
+
+    //<editor-fold desc="Support methods">
+    private ResponseEntity getResponse(MultipartException ex) {
+        Throwable root = ex.getMostSpecificCause();
+        String message = root instanceof FileUploadBase.FileSizeLimitExceededException
+                ? String.format("The file exceeds its maximum permitted size \"%s\"", ((FileUploadBase.FileSizeLimitExceededException) root).getPermittedSize())
+                : ex.getMessage();
+        return badRequest().body(message);
+    }
+    //</editor-fold>
 }
