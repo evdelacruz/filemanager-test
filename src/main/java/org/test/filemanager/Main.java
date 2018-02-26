4ebdee563e461062a1674a3afd678183dd5b66c9
evdelacruz <evdelacruz@nauta.cu>
2018-02-26 11:15:56 -0500

    Added controller customizations (swagger).

--- src/main/java/org/test/filemanager/Main.java
+++ src/main/java/org/test/filemanager/Main.java
@@ -2,14 +2,14 @@ package org.test.filemanager;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.PathSelectors.ant;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static org.springframework.boot.SpringApplication.run;
 import static java.util.Collections.emptyList;
 
@@ -25,10 +25,11 @@ public class Main {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.test.filemanager.controller"))
-                .paths(PathSelectors.any())
+                .apis(basePackage("org.test.filemanager.controller"))
+                .paths(ant("/api/file/**"))
                 .build()
-                .apiInfo(this.info());
+                .apiInfo(this.info())
+                .useDefaultResponseMessages(false);
     }
 
     //<editor-fold desc="Support methods">
@@ -38,7 +39,7 @@ public class Main {
                 "Test example of file management",
                 "1.0",
                 null,
-                new Contact("Erick Vega","https://github.com/evdelacruz",null),
+                new Contact("Erick Vega",null,null),
                 "Apache 2.0",
                 "https://github.com/evdelacruz/filemanager-test/blob/master/LICENSE",
                 emptyList()
